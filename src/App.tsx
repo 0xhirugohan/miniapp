98ccdca843d88bb9b71b2802ffee885a31051f94
han <han@fbrns.co>
2025-12-17 14:43:20 +0700

    feat: initiate Farcaster SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { sdk } from "@farcaster/miniapp-sdk";
+
 import { APITester } from "./APITester";
 import { getGitCommitHash } from "./getGitCommitHash.ts" with { type: "macro" };
 import "./index.css";
@@ -7,6 +10,18 @@ import reactLogo from "./react.svg";
 
 export function App() {
   const gitCommitHash = getGitCommitHash();
+
+  const initiateFarcasterSDK = async () => {
+    try {
+      await sdk.actions.ready();
+    } catch (err) {
+      console.error("Fail to initiate Farcaster SDK");
+    }
+  }
+
+  useEffect(() => {
+    initiateFarcasterSDK();
+  }, []);
 
   return (
     <div className="app">
